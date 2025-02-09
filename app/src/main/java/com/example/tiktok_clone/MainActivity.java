a9ba34136707535c58ba79595e8751e514927204
Trần Quang Đạo <101162243+DAOROBOT@users.noreply.github.com>
2025-02-09 14:22:23 +0700

    Changes in a MainActivity.java file

--- app/src/main/java/com/example/tiktok_clone/MainActivity.java
+++ app/src/main/java/com/example/tiktok_clone/MainActivity.java
@@ -1,24 +1,44 @@
 package com.example.tiktok_clone;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private VideoAdapter videoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        viewPager = findViewById(R.id.viewPager);
+        List<String> videoUrls = new ArrayList<>();
+        videoUrls.add("android.resource://" + getPackageName() + "/" + R.raw.video1);
+        videoUrls.add("android.resource://" + getPackageName() + "/" + R.raw.video2);
+        videoUrls.add("android.resource://" + getPackageName() + "/" + R.raw.video3);
+
+        videoAdapter = new VideoAdapter(videoUrls);
+        viewPager.setAdapter(videoAdapter);
+
+        // Tự động phát video khi chuyển trang
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                videoAdapter.playVideo(position);
+            }
         });
     }
 }
